f4572462185ed7a98bfc2d7f2bee1d965bfd4db5
prasiddhigyawali <65633555+prasiddhigyawali@users.noreply.github.com>
2023-02-08 11:49:03 -0700

    reading file

--- ASADirectory.py
+++ ASADirectory.py
@@ -1,4 +1,9 @@
 import pandas
 
 if __name__ == "__main__":
-    
+    memeberDirectory = []
+    with open('sample.txt') as f:
+        lines = f.readlines()
+    for l in lines:
+        member = l.split(" ");
+        memeberDirectory.append(member)
