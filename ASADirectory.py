543219d5e9e711a75772b3ff82f63c7cafd296b9
prasiddhigyawali <65633555+prasiddhigyawali@users.noreply.github.com>
2023-05-12 00:41:52 -0700

    final

--- ASADirectory.py
+++ ASADirectory.py
@@ -1,37 +1,36 @@
 import csv
+import re
 
 if __name__ == "__main__":
     memeberDirectory = []
-    with open('sample_clean.txt', 'r', errors='replace') as f:
+    with open('/Users/prasiddhigyawali/ASADirectory/sample.txt', 'r', errors='replace') as f:
         lines = f.readlines()
-    with open('directory_test.csv', 'a') as file:
-        dw = csv.DictWriter(file, delimiter=',', fieldnames=["LAST NAME", "FIRST NAME", "ADDRESS", "SECTION"])
+    with open('/Users/prasiddhigyawali/ASADirectory/directory_test.csv', 'a') as file:
+        dw = csv.DictWriter(file, delimiter=',', fieldnames=["NAME", "ADDRESS", "SECTION", "ERROS"])
         dw.writeheader()
     for l in lines:
-        if(l[len(l)-1] == '\n'):
-            l = l[:len(l)-1]
-        member = l.split(".");
-        memeberDirectory.append(member)
-    for i in memeberDirectory:
+        name = ""
         address = ""
-        section = ""
-        for j in range (2, len(i)):
-            if("SECT" not in i[j]):
-                address += i[j]
-            else:
-                adjust = i[j].split("SECT")
-                address += adjust[0]
-                section = adjust[1]
-        print(f"{address}\n")
-        if(section != ""):
-            with open('directory_test.csv', 'a') as file:
+        sect = "" 
+        errors = ""
+        for i in range(0,len(l)):
+            if(l[i].isalpha() is False and l[i] != '-' and l[i] != '.' and l[i] != ' ' and l[i] != ','):
+                name = l[:i]
+                address = l[i:]
+                break
+        if("SECT" in address):
+            temp = address.split("SECT")
+            address = temp[0];
+            sect = temp[1]
+        if(re.search("[A-Za-b.\- ]", name) or name == ""):
+           errors += "1 "
+        if(re.search("[A-Za-b. ]", address)):
+           errors += "2 "
+        if(sect.strip() != "" and sect.isnumeric() is False):
+           errors += "3 "
+        with open('/Users/prasiddhigyawali/ASADirectory/directory_test.csv', 'a') as file:
                 writer = csv.writer(file)
-                writer.writerow([i[0], i[1], address, section])
+                writer.writerow([name, address, sect, errors])
                 file.close()
-            print(f"{section}\n")
-        else:
-            with open('directory_test.csv', 'a') as file:
-                writer = csv.writer(file)
-                writer.writerow([i[0], i[1], address])
-                file.close()
+
     f.close()
